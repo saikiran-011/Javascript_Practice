2698b757262f9e80e6c4de204ebfe75c715c62d0
Sai Kiran Budumuru <saikiranb117@gmail.com>
2025-09-14 18:35:24 +0530

    Update First.js

--- Javascript/First.js
+++ Javascript/First.js
@@ -1,2 +1,32 @@
 console.log('Hello');
 console.log("Everyone");
+
+let num = 100.5;
+console.log(typeof num);
+
+let name = "sai";
+console.log(typeof name);
+
+const num1 = 1234567890987654321n;
+console.log(typeof num1);
+
+const value = true;
+console.log(typeof value);
+
+const check = Boolean(0);
+console.log(check);
+
+let no_value;
+console.log(no_value);
+
+const value1 = null;
+console.log(value1);
+
+const obj = {
+  name : 'sai',
+  age : 24,
+  gender : "male"
+}
+
+console.log(obj);
+
