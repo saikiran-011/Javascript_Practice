3f394de8bd299feb2e73d3c1b08ef4dcc56fd9f6
Sai Kiran Budumuru <saikiranb117@gmail.com>
2026-03-31 11:12:22 +0530

    day 1 Practice

---
+++ Javascript/day1.js
@@ -0,0 +1,51 @@
+/* First Code */ 
+
+console.log("Hello Everyone");
+console.log("My Name is Sai Kiran Budumuru");
+
+/*
+console is a built-in object provided by the runtime (browser/Node)
+that allows JavaScript to send output for debugging and inspection. 
+
+--console is a object 
+--log is function of the console object (it sends the data to terminal if using Node Js or Browser console if using browser)
+*/
+
+console.log(console);
+
+/* 
+Output is
+
+Object [console] {
+  log: [Function: log],
+  info: [Function: info],
+  debug: [Function: debug],
+  warn: [Function: warn],
+  error: [Function: error],
+  dir: [Function: dir],
+  time: [Function: time],
+  timeEnd: [Function: timeEnd],
+  timeLog: [Function: timeLog],
+  trace: [Function: trace],
+  assert: [Function: assert],
+  clear: [Function: clear],
+  count: [Function: count],
+  countReset: [Function: countReset],
+  group: [Function: group],
+  groupEnd: [Function: groupEnd],
+  table: [Function: table],
+  dirxml: [Function: dirxml],
+  groupCollapsed: [Function: groupCollapsed],
+  Console: [Function: Console],
+  profile: [Function: profile],
+  profileEnd: [Function: profileEnd],
+  timeStamp: [Function: timeStamp],
+  context: [Function: context],
+  createTask: [Function: createTask]
+}
+*/ 
+
+/* Variables 
+
+A variable is like a container that stores data so you can use it later in your program.
+*/
