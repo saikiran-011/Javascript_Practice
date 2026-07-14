7beb302dd1306c5bf221e73931db01907123f371
Sai Kiran Budumuru <saikiranb117@gmail.com>
2026-07-14 23:01:34 +0530

    Fundamentals done

--- Javascript/JS_fundamentals.js
+++ Javascript/JS_fundamentals.js
@@ -1,7 +1,21 @@
 
-//comparison operators
+let numbers;
 
-let x = 5;
-let y = "5";
-console.log(x == y);    // equality operator
-console.log(x === y);   // strict equality operator
+numbers = 22;
+
+console.log(numbers);
+
+
+
+var myName;   // Declaration of variable
+
+myName = "Sai Kiran";  // Initialization of variable 
+
+console.log(myName);
+
+
+
+const PI = 3.14; // Const should be declared and initialize in the same line.
+
+console.log(PI); // Const variables cannot be changed. A const variable can be initialized only once. After that, it cannot be assigned a new value.
+
--- Javascript/day1.js
+++ Javascript/day1.js
@@ -92,23 +92,3 @@ Rules are for the computer. Conventions are for humans.
 */
 
 
-let numbers;
-
-numbers = 22;
-
-console.log(numbers);
-
-
-
-var myName;   // Declaration of variable
-
-myName = "Sai Kiran";  // Initialization of variable 
-
-console.log(myName);
-
-
-
-const PI = 3.14; // Const should be declared and initialize in the same line.
-
-console.log(PI); // Const variables cannot be changed. A const variable can be initialized only once. After that, it cannot be assigned a new value.
-
