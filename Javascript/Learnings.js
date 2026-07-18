b7e6b708652d45999995d0a2ff2911a394adc031
Sai Kiran Budumuru <saikiranb117@gmail.com>
2026-07-18 23:38:05 +0530

    Learnings

---
+++ Javascript/Learnings.js
@@ -0,0 +1,31 @@
+
+
+/*    
+
+Programming languages are designed for humans. Machine instructions are designed for computers.
+--> Programming languages make it easier for humans to communicate instructions to computers.
+
+Programming languages are designed by humans to solve human problems. Keywords are design choices, not universal truths.
+--> Keywords like var, let, if, and function are chosen by language designers. Another language could choose completely different words.
+
+Programming languages evolve because software evolves.
+--> New features are usually introduced because developers encounter new problems—not because the old features suddenly became useless.
+
+One of the biggest sources of software bugs is unexpected changes to shared data.
+--> Programs become unreliable when data changes unexpectedly.
+
+The problem is rarely that data can change. The real problem is uncontrolled access to that data.
+--> Allowing anyone, anywhere, to change it is what causes bugs.
+
+A variable should exist only where it is needed and be visible only where it is needed.
+-->This keeps programs:
+
+    Easier to understand
+    Easier to maintain
+    Less likely to contain bugs
+
+Functions create their own scope. if, for, and while blocks do not create a new scope for var.
+    
+
+
+*/
