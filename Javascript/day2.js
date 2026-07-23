8c437c2bbda982d147e4c96057841acb8c743afa
Sai Kiran Budumuru <saikiranb117@gmail.com>
2026-07-23 23:16:05 +0530

    Day 2

---
+++ Javascript/day2.js
@@ -0,0 +1,26 @@
+
+
+// JavaScript doesn't execute your code in one phase.
+
+
+/*
+
+        Source Code
+            │
+            ▼
+        ┌─────────────────────────┐
+        │ Phase 1                 │
+        │ Memory Creation Phase   │
+        └─────────────────────────┘
+            │
+            ▼
+        ┌─────────────────────────┐
+        │ Phase 2                 │
+        │ Code Execution Phase    │
+        └─────────────────────────┘
+
+*/
+
+
+
+
